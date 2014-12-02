dceea0d8c632d1c46aef3a1848367283f1dfb6d1
Aurelio Canto <n.jarzombek05@gmail.com>
2014-12-02 14:56:44 -0600

    Aplicación Finalizada

--- app/src/main/java/aar/businessinspain/Details.java
+++ app/src/main/java/aar/businessinspain/Details.java
@@ -30,68 +30,99 @@ public class Details extends Activity {
 
         if ( mes == (R.id.imageViewModelo) )
         {
-            ((TextView)findViewById(R.id.textViewTitle)).setText("Modelo");
+            ((TextView)findViewById(R.id.textViewTitle)).setText("Modelo de Comunicación");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.modelo);
             ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.modelo_comunicacion_info)) ;
         }
         else if ( mes == (R.id.imageViewVestimenta) )
         {
-            ((TextView)findViewById(R.id.textViewTitle)).setText("Vestimenta");
+            ((TextView)findViewById(R.id.textViewTitle)).setText("Código de Vestimenta");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.vestimenta);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.codigo_vestimenta_info)) ;
         }
         else if ( mes == (R.id.imageViewAgenda) )
         {
             ((TextView)findViewById(R.id.textViewTitle)).setText("Agenda");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.agenda);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.agenda_info)) ;
         }
         else if ( mes == (R.id.imageViewAspectos) )
         {
-            ((TextView)findViewById(R.id.textViewTitle)).setText("Aspectos");
+            ((TextView)findViewById(R.id.textViewTitle)).setText("Aspectos Generales");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.generales);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.aspectos_generales_info)) ;
         }
         else if ( mes == (R.id.imageViewAlimentos) )
         {
             ((TextView)findViewById(R.id.textViewTitle)).setText("Alimentos");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.alimentos);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.horario_alimentos_info)) ;
         }
         else if ( mes == (R.id.imageViewRecomendaciones) )
         {
             ((TextView)findViewById(R.id.textViewTitle)).setText("Recomendaciones");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.recomendaciones);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.recomendaciones_info)) ;
         }
         else if ( mes == (R.id.imageViewInternet) )
         {
-            ((TextView)findViewById(R.id.textViewTitle)).setText("Internet");
+            ((TextView)findViewById(R.id.textViewTitle)).setText("Telefonía e Internet");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.internet);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.modelo_comunicacion_info)) ;
         }
         else if ( mes == (R.id.imageViewInteres) )
         {
-            ((TextView)findViewById(R.id.textViewTitle)).setText("Interes");
+            ((TextView)findViewById(R.id.textViewTitle)).setText("Lugares de Interés");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.interes);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.modelo_comunicacion_info)) ;
         }
         else if ( mes == (R.id.imageViewAerolineas) )
         {
-            ((TextView)findViewById(R.id.textViewTitle)).setText("Aerolineas");
+            ((TextView)findViewById(R.id.textViewTitle)).setText("Aerolíneas");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.aerolineas);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.modelo_comunicacion_info)) ;
         }
         else if ( mes == (R.id.imageViewSalud) )
         {
             ((TextView)findViewById(R.id.textViewTitle)).setText("Salud");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.salud);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.modelo_comunicacion_info)) ;
         }
         else if ( mes == (R.id.imageViewClima) )
         {
             ((TextView)findViewById(R.id.textViewTitle)).setText("Clima");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.clima);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.modelo_comunicacion_info)) ;
         }
         else if ( mes == (R.id.imageViewVisa) )
         {
-            ((TextView)findViewById(R.id.textViewTitle)).setText("Visa");
+            ((TextView)findViewById(R.id.textViewTitle)).setText("Visa y Pasaporte");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.visa);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.modelo_comunicacion_info)) ;
         }
         else if ( mes == (R.id.imageViewAranceles) )
         {
-            ((TextView)findViewById(R.id.textViewTitle)).setText("Aranceles");
+            ((TextView)findViewById(R.id.textViewTitle)).setText("Trámites Arancelarios");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.aranceles);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.modelo_comunicacion_info)) ;
         }
         else if ( mes == (R.id.imageViewEmbajada) )
         {
             ((TextView)findViewById(R.id.textViewTitle)).setText("Embajada");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.embajada);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.modelo_comunicacion_info)) ;
         }
         else if ( mes == (R.id.imageViewConsulados) )
         {
             ((TextView)findViewById(R.id.textViewTitle)).setText("Consulados");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.consulados);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.modelo_comunicacion_info)) ;
         }
         else if ( mes == (R.id.imageViewTelefonos) )
         {
-            ((TextView)findViewById(R.id.textViewTitle)).setText("Telefonos");
+            ((TextView)findViewById(R.id.textViewTitle)).setText("Teléfonos de Emergencia");
+            ((ImageView) findViewById(R.id.imageViewIcon)).setImageResource(R.drawable.telefonos);
+            ((TextView)findViewById(R.id.textViewDescripcion)).setText(getText(R.string.modelo_comunicacion_info)) ;
         }
 
 
--- app/src/main/java/aar/businessinspain/MenuGeneral.java
+++ app/src/main/java/aar/businessinspain/MenuGeneral.java
@@ -1,26 +1,24 @@
 package aar.businessinspain;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MenuSpain extends Activity {
+public class MenuGeneral extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_spain);
+        setContentView(R.layout.activity_menu_general);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu_spain, menu);
+        getMenuInflater().inflate(R.menu.menu_menu_general, menu);
         return true;
     }
 
@@ -37,28 +35,5 @@ public class MenuSpain extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void advanceToInfo(View view) {
-
-        Intent intent = new Intent(this, MenuInfo.class);
-        startActivity(intent);
-    }
-
-    public void advanceToProtocolo(View view) {
-
-        Intent intent = new Intent(this, MenuProtocolo.class);
-        startActivity(intent);
-
-    }
-
-    public void advanceToEmergencias(View view) {
-        Intent intent = new Intent(this, MenuEmergencias.class);
-        startActivity(intent);
-    }
-
-    public void advanceToGeneral(View view) {
-        Intent intent = new Intent(this, MenuGeneral.class);
-        startActivity(intent);
     }
 }
